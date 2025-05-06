6b4ec5149b7a0bfe6bae4a6653d68a7a36d2123c
Mohamed Ilham <im.ilhamk@gmail..com>
2025-05-06 13:33:18 +0530

    Implement User model class with properties and fromJson factory constructor

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,17 +1,44 @@
-// TODO: Implement User model class
-// Requirements:
-// - Create a model class to represent user data
-// - Implement fromJson factory constructor
-// - Include properties for name, email, phone, website, etc.
+import 'package:flutter/foundation.dart';
 
 class User {
-  // TODO: Add properties
-  
-  // TODO: Add constructor parameters
-  
-  // TODO: Implement fromJson factory constructor
+  final int id;
+  final String name;
+  final String email;
+  final String phone;
+  final String website;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.website,
+  });
+
   factory User.fromJson(Map<String, dynamic> json) {
-    // TODO: Parse JSON and return User instance
-    throw UnimplementedError('User.fromJson has not been implemented');
+    return User(
+      id: json['id'] as int,
+      name: json['name'] as String? ?? 'N/A',
+      email: json['email'] as String? ?? 'N/A',
+      phone: json['phone'] as String? ?? 'N/A',
+      website: json['website'] as String? ?? 'N/A',
+    );
   }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is User &&
+          runtimeType == other.runtimeType &&
+          id == other.id &&
+          name == other.name &&
+          email == other.email &&
+          phone == other.phone &&
+          website == other.website;
+
+  @override
+  int get hashCode => Object.hash(id, name, email, phone, website);
+
+  @override
+  String toString() => 'User(id: $id, name: $name, email: $email, phone: $phone, website: $website)';
 }
